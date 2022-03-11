ad3f46d9cf0660049da85ccf8c1b6ececcba67fe
sekikazuya <88357511+sekikazuya@users.noreply.github.com>
2022-03-11 11:35:51 +0900

    feat Vue.js setup

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,20 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+// import Vuetify from 'vuetify';
+// import 'vuetify/dist/vuetify.min.css';
+
+import { createApp } from 'vue';
+// Vue.use(Vuetify);
+
+const app = createApp({
+  components: {
+    'sample-component': require('./components/Sample.vue').default,
+  }
+}).mount('#app')
+// Vue.component('sample-component', require('./components/Sample.vue').default);
+
+// const app = new Vue({
+//   el : '#app',
+// })
---
+++ src/resources/js/components/Sample.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>SampleComponent</h1>
+    <ul>
+      <li>calender</li>
+      <li>month</li>
+      <li>week</li>
+    </ul>
+  </div>
+</template>
